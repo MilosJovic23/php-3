f60c90b479a88afdb8cdf1f6afb1c39362380ca9
MilosJovic23 <milos.mj992@gmail.com>
2024-10-28 12:11:27 +0100

    * small changes

--- index.php
+++ index.php
@@ -6,8 +6,59 @@
     $informacie = ["Milos","Jovic",20];
 
 
-    echo $informacie[0];
 
 
+    $osoba = [
+
+        "ime" => "Milos",
+        "prezime" => "Jovic",
+    ];
+
+//    echo $osoba["ime"];
+//    echo $osoba["prezime"];
+
+
+
+
+
+    $automobili = [
+        "Zastava" => ["yugo","101","skala"]
+    ];
+
+    echo $automobili["Zastava"][0];
+
+    $osobe = [
+        "Toma" =>[
+            "prezime" => "Nikolic",
+            "godine" => 29,
+            "programer" => true,
+            "pravoIme" => "Tomislav"
+        ],
+        "Milos" => [
+            "prezime" => "Jovic",
+            "godine" => 32,
+            "programer" => true,
+        ]
+    ];
+
+    echo "<br>" . $osobe["Toma"]["prezime"];
+    echo "<br>" . $osobe["Milos"]["prezime"];
+
+    $osobe["Jelena"]=[
+        "prezime"=>"Nikolic",
+        "godine"=>31,
+        "programer"=>true];
+
+
+//    echo "ja se zovem {$osobe['Jelena']['prezime']}";
+
+
+//    $imena = array_keys($osobe);
+
+//    var_dump($imena);
+
+//    $prezime = array_column($osobe,"prezime");
+//
+//    var_dump($prezime);
 
 ?>
